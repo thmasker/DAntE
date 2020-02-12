23866550ea51e7df02247d53b1a8478aa63585b2
thmasker <diego.p.hidalgo@gmail.com>
2020-02-12 11:27:05 +0100

    Add singleton pattern to database access

---
+++ src/dbbroker.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+HOST = '161.67.142.141'
+PORT = 27017
+DB = 'differential_uclm_db'
+
+class DBBroker:
+	__instance = None
+
+	@staticmethod 
+	def getInstance() -> MongoClient:
+		if DBBroker.__instance == None:
+			DBBroker()
+		return DBBroker.__instance
+	
+	def __init__(self, host: str = HOST, port: int = PORT, db: str = DB):
+		if DBBroker.__instance != None:
+			raise Exception("This class is a singleton")
+		else:
+			DBBroker.__instance = MongoClient(host, port)[db]
