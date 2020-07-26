d7bd707ced8fbbdb296d8e25fa85f6499460f5ad
thmasker <diego.p.hidalgo@gmail.com>
2020-07-26 20:42:42 +0200

    Some improvements on data preprocessing

--- src/data_preprocessing/weekday_prototypes.py
+++ src/data_preprocessing/weekday_prototypes.py
@@ -1,28 +1,42 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import pickle
+
+from typing import Tuple
 
 
 OUT_PATH = 'C:/Users/thmas/OneDrive - Universidad de Castilla-La Mancha/Informática/TFG/out/'
 
 
-def get_prototype(df: pd.DataFrame, mode: str = 'mean') -> pd.DataFrame:
+def getBoxPlot(df: pd.DataFrame) -> plt.Figure:
+    df[['h' + str(i) for i in range(24)]] = df.apply(lambda x: x[-1], axis=1, result_type='expand')
+    df = df.drop(['building_id', 'weekday', 'active', 'type', 'consumptions'], axis=1)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+
+    df.plot(kind='box', figsize=(12,10), ax=ax)
+    df.mean().plot(ax=ax, color='red')
+    
+    return fig
+
+
+def get_prototype(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
     weekday = df['weekday'].iloc[0]
     active = df['active'].iloc[0]
     consumer_type = df['type'].iloc[0]
 
-    cons = []
+    mean, std = [], []
     for i in range(24):
         i_consumptions = []
         for j in range(df.shape[0]):
             i_consumptions.append(df['consumptions'].iloc[j][i])    # i_consumptions correspond to the ith hour
 
-        if mode == 'std':
-            cons.append(np.nanstd(i_consumptions))
-        else:
-            cons.append(np.nanmean(i_consumptions))
+        std.append(np.nanstd(i_consumptions))
+        mean.append(np.nanmean(i_consumptions))
 
-    return pd.DataFrame({'weekday': weekday, 'active': active, 'type': consumer_type, 'consumptions': [cons]})
+    return pd.DataFrame({'weekday': weekday, 'active': active, 'type': consumer_type, 'consumptions': [mean]}), pd.DataFrame({'weekday': weekday, 'active': active, 'type': consumer_type, 'consumptions': [std]})
 
 
 if __name__ == '__main__':
@@ -36,9 +50,9 @@ if __name__ == '__main__':
             df_a = df[df['active'] == a]
 
             for t in df_a['type'].unique():
-                df_t = df_a[df_a['type'] == t]
+                mean, std = get_prototype(df_a[df_a['type'] == t])
 
-                mean_proto = mean_proto.append(get_prototype(df_t, mode='mean'), ignore_index=True)
-                std_proto = std_proto.append(get_prototype(df_t, mode='std'), ignore_index=True)
+                mean_proto = mean_proto.append(mean, ignore_index=True)
+                std_proto = std_proto.append(std, ignore_index=True)
 
     mean_proto.to_pickle(OUT_PATH + 'prototypesMEAN.zip', compression='zip'), std_proto.to_pickle(OUT_PATH + 'prototypesSTD.zip', compression='zip')
